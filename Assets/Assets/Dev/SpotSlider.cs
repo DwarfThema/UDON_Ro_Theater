6b81b66d72053740ebc4409a68073d93b4fc021f
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2022-07-02 17:18:43 +0900

    Add project files.

---
+++ Assets/Assets/Dev/SpotSlider.cs
@@ -0,0 +1,27 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+public class SpotSlider : UdonSharpBehaviour
+{
+    public Animator spotAnim;
+    public string SpotLightParam;
+    public Slider slider;
+    [UdonSynced, SerializeField] float sliderValue;
+
+    public void spotSlider()
+    {
+        spotAnim.SetFloat(SpotLightParam, slider.value);
+        sliderValue = slider.value;
+        RequestSerialization();
+    }
+
+    public override void OnDeserialization()
+    {
+        slider.value = sliderValue;
+    }
+
+}
---
+++ Assets/Assets/Dev/Udon/Udon_Barricade.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Udon_Barricade : UdonSharpBehaviour
+{
+    public GameObject barricade_mesh;
+    [UdonSynced, SerializeField] bool netWorkBool = true;
+
+    public override void Interact()
+    {
+        if (Networking.IsMaster)
+        {
+            barricade_mesh.SetActive(!barricade_mesh.activeInHierarchy);
+            netWorkBool = barricade_mesh.activeInHierarchy;
+            RequestSerialization();
+        }
+    }
+
+    public override void OnDeserialization()
+    {
+        barricade_mesh.SetActive(netWorkBool);
+    }
+
+}
