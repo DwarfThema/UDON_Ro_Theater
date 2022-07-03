3cf62ad34eddb2d71dcfe139c934e6801a430f78
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2022-07-03 14:21:32 +0900

    IsMaster

---
+++ Assets/Assets/Dev/Udon/IsMaster.cs
@@ -0,0 +1,31 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class IsMaster : UdonSharpBehaviour
+{
+    public int heightPos = 1;
+
+    void Start()
+    {
+        if (Networking.IsMaster)
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Networking.IsMaster)
+        {
+            VRCPlayerApi.TrackingData tData = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head);
+            transform.position = new Vector3(tData.position.x, tData.position.y + heightPos, tData.position.z);
+        }
+    }
+}
